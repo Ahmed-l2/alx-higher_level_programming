e516fa441432f84aec883542affd6fd682ce286b
Ahmed <ahmed_l2@yahoo.com>
2023-11-29 04:12:15 +0300

    15. Remove at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    num = 0
-    for char in str:
-        if num == n:
-            print("", end="")
-        else:
-            print(char, end="")
-        num += 1
+    print(str[:n], end="")
+    print(str[n + 1:])

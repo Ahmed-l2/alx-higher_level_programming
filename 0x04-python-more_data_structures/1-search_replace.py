181c25d0a723e152d897b23d0b438ed9d747c076
Ahmed <ahmed_l2@yahoo.com>
2023-12-06 22:55:54 +0300

    1. Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if not my_list:
+        return None
+   new_list = []
+   for num in my_list:
+       if num == search:
+           new_list.append(replace)
+       else:
+           new_list.append(num)
+    return new_list

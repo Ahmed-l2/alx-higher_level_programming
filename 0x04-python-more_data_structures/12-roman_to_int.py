0864d726352bc3d270f3226e671f605c09d5df9e
Ahmed <ahmed_l2@yahoo.com>
2023-12-07 04:53:16 +0300

    12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is not str and not roman_string:
+    if roman_string is not str and roman_string is None:
         return None
     rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     value = 0

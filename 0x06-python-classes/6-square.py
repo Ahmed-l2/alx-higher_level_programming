d34404032c65dc822f4a5149d28197f9d72e1cb0
Ahmed <ahmed_l2@yahoo.com>
2023-12-20 02:48:14 +0300

    6. Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,7 +23,7 @@ class Square:
             int: The area of the square, computed as the square of the side
             length.
         """
-        return (self.__size ** 2)
+        return (self.__size * self.__size)
 
     @property
     def size(self):

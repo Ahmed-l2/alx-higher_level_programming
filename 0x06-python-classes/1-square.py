abd1a73db6fa4fe82e62cd30ec6bfedfdde7dfc7
Ahmed <ahmed_l2@yahoo.com>
2023-12-19 21:57:38 +0300

    1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,9 +6,9 @@ class Square:
     """A class representing a square."""
     
     def __init__(self, size):
-        """Constructor
+        """Constructor.
 
         Args:
-            size: Size atribute of the square
+            size: Size atribute of the square.
         """
         self._size = size

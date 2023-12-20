6c3ba38580e1f28675839fce737e8b3f61ae2671
Ahmed <ahmed_l2@yahoo.com>
2023-12-20 05:09:50 +0300

    10. ByteCode -> Python #5

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -15,13 +15,13 @@ class MagicClass:
         """
         self.__radius = 0
         if type(radius) is not int or type(radius) is not float:
-            raise TypeError("radius must be number")
+            raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-        """returns the area of the circle"""
+        """returns the area of the circle."""
         return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-        """returns the circumference of the circle"""
+        """returns the circumference of the circle."""
         return (2 * math.pi * self.__radius)

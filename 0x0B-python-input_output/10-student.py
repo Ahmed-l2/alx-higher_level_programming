b574334224b3f3cf2d3d9177b0400320b156166f
Ahmed <ahmed_l2@yahoo.com>
2024-01-09 21:37:57 +0300

    10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module for Student Class"""
+
+
+class Student:
+    """Student Class representing first and last name and age."""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        try:
+            for attribute in attrs:
+                if type(attribute) is not str:
+                    return vars(self)
+        except Exception:
+            return vars(self)
+
+        st_dict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                st_dict[key] = value
+        return st_dict

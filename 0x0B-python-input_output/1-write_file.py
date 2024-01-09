03eabba9a52032366459fa6e3ba3e816d1a46876
Ahmed <ahmed_l2@yahoo.com>
2024-01-09 18:51:45 +0300

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -10,5 +10,5 @@ def write_file(filename="", text=""):
         text: text to be written to file
     """
 
-    with open(filename, encoding='utf-8') as file:
+    with open(filename, encoding='utf-8', 'w') as file:
         file.write(text)

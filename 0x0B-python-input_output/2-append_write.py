937b93b1952c424110a846f73be3648c4840593a
Ahmed <ahmed_l2@yahoo.com>
2024-01-09 18:55:54 +0300

    1. Write to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""append_file function"""
+
+
+def append_file(filename="", text=""):
+    """appends text to filename with encoding UTF-8
+
+    Args:
+        filename: Name of the file to be read
+        text: text to be appened to file
+    """
+
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.append(text)

1215e37229a2f00758c0ab894e111d70d1e8f1d2
Ahmed <ahmed_l2@yahoo.com>
2024-01-09 18:50:51 +0300

    1. Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""write_file function"""
+
+
+def write_file(filename="", text=""):
+    """writes to filename with encoding UTF-8
+
+    Args:
+        filename: Name of the file to be read
+        text: text to be written to file
+    """
+
+    with open(filename, encoding='utf-8') as file:
+        file.write(text)

3349504f3d88e3b9e81c3ff3a66e5db6c819db76
Ahmed <ahmed_l2@yahoo.com>
2024-01-09 21:12:53 +0300

    8. Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for class_to_json"""
+
+def class_to_json(obj):
+    """Creates dictionary description with simple data structure (list, dict,
+    string, integer and boolean) for JSON serialization of an object
+
+    Args:
+        obj: given object
+
+    Returns:
+        JSON serialization of an object
+    """
+
+    return vars(obj)

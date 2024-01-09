c8907a83fc4217514c3644bd74e54a16e0871866
Ahmed <ahmed_l2@yahoo.com>
2024-01-10 00:44:54 +0300

    13. Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Function definition for append_after"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text to a file, after each line containing
+    a specific string"""
+
+    new_content = []
+
+    with open(filename, 'r') as file:
+        for line in file:
+            if search_string in line:
+                new_content.append(line.strip() + '\n' + new_string)
+            else:
+                new_content.append(line)
+
+    new_content = "".join(new_content)
+
+    with open(filename, 'w') as file:
+        file.write(new_content)

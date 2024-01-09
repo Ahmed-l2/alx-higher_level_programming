4c599a7064e19b354cf86e6946ad66062a7cbe1f
Ahmed <ahmed_l2@yahoo.com>
2024-01-10 00:58:24 +0300

    13. Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -13,10 +13,9 @@ def append_after(filename="", search_string="", new_string=""):
             line = file.readline()
             if line == "":
                 break
-            for line in file:
-                if search_string in line:
-                    new_content.append(line.strip() + '\n' + new_string)
-                else:
+            if search_string in line:
+                new_content.append(line.strip() + '\n' + new_string)
+            else:
                     new_content.append(line)
 
     with open(filename, 'w', encoding='utf-8') as file:

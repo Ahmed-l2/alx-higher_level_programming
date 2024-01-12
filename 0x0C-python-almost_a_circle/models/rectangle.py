72ac9e5f4b05a5cfe680b2d756743d647ed6ee2e
Ahmed <ahmed_l2@yahoo.com>
2024-01-12 23:41:35 +0300

    3. Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,10 +27,10 @@ class Rectangle(Base):
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -38,6 +38,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        self.validate_integer("width", value, False)
         self.__width = value
 
     @property
@@ -46,6 +47,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        self.validate_integer("height", value, False)
         self.__height = value
 
     @property
@@ -54,6 +56,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        self.validate_integer("x", value)
         self.__x = value
 
     @property
@@ -62,4 +65,13 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        self.validate_integer("y", value)
         self.__y = value
+
+    def validate_integer(self, attr, value, eq=True):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(attr))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(attr))
+        elif not eq and value < 0:
+            raise ValueError("{} must be > 0".format(attr))

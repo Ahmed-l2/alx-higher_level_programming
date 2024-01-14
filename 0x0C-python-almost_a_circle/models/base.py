ef010fb8607bd3f0f5446d040e188c57b520f70b
Ahmed <ahmed_l2@yahoo.com>
2024-01-14 19:28:55 +0300

    19. File to instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,14 +35,23 @@ class Base:
             file.write(cls.to_json_string(list_objs))
 
     def from_json_string(json_string):
-        """eturns the list of the JSON string representation json_string."""
+        """Returns the list of the JSON string representation json_string."""
         if json_string is None:
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """eturns an instance with all attributes already set"""
+        """Returns an instance with all attributes already set"""
         dummy_instance = cls(1, 1)
         dummy_instance.update(**dictionary)
         return dummy_instance
+
+    @classmethod
+    def load_from_file(cls):
+        from os import path
+        file = "{}.json".format(cls.__name__)
+        if not path.isfile(file):
+            return []
+        with open(file, 'r', encoding="utf-8") as f:
+            return [cls.create(**dic) for dic in cls.from_json_string(f.read())]

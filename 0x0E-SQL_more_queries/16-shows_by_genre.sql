d0cb3ea00214320e918084fcf274362aa384def0
Ahmed <ahmed_l2@yahoo.com>
2024-02-15 01:04:13 +0300

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,6 +2,6 @@
 -- from the database hbtn_0d_tvshows.
 SELECT tv_shows.title, IFNULL(tv_genres.name, NULL) as genre_name
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;

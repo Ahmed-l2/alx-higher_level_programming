3b2a453b9e462fac8da9248dcbf9810cd9907fe3
Ahmed <ahmed_l2@yahoo.com>
2024-02-15 01:02:48 +0300

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- ists all shows, and all genres linked to that show,
 -- from the database hbtn_0d_tvshows.
-SELECT tv_shows.title, tv_genres.name
+SELECT tv_shows.title, IFNULL(tv_genres.name, NULL) as genre_name
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id

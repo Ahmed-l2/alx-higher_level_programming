36629e829ffc48d929bc604d6b53136f452bfea0
Ahmed <ahmed_l2@yahoo.com>
2024-02-14 21:45:56 +0300

    6. States table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- creates the database hbtn_0d_usa and the table states
+-- (in the database hbtn_0d_usa)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states
+(id INT UNIQUE AUTO_INCRIMENT NOT NULL PRIMARY KEY,
+name VARCHAR(256) NOT NULL);

f55f2d50f10e10f819c7162a06b8671020568423
Ahmed <ahmed_l2@yahoo.com>
2024-02-14 23:41:44 +0300

    14. My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,6 +2,6 @@
 SELECT tv_genres.name
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genre.id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'DEXTER'
 ORDER BY tv_genres.name ASC;

bf5a426268bab694fee35dae49ade3574b534018
Ahmed <ahmed_l2@yahoo.com>
2024-02-15 00:35:25 +0300

    15. Only Comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,6 +2,6 @@
 SELECT tv_shows.title
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
-JOIN tv_shows ON tv_show_genres.genre_id = tv_shows.id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE name = 'Comedy'
 ORDER BY tv_shows.title ASC;

ba77184cbc90c83565092492a897e33a7eb17540
Ahmed <ahmed_l2@yahoo.com>
2024-02-14 05:59:53 +0300

    .

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,5 @@
 -- displays the average temperature (Fahrenheit) by city ordered by temperature
-select city, avg(value) as avg_temp from temperatures group by city order by
-avg_temp desc;
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,5 @@
 -- lists the number of records with the same score in the table second_table
-select score, count(*) as number from second_table group by score order by
-number desc;
+SELECT score, COUNT('score') as number
+FROM second_table
+GROUP BY score
+ORDER BY score DESC;

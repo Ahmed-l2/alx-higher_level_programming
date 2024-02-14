59ff24f4adf5900dbe2859627b354bd6d3908131
Ahmed <ahmed_l2@yahoo.com>
2024-02-14 05:59:12 +0300

    .

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table of the database hbtn_0c_0
-select score, name from second_table order by score desc;
+SELECT score, name FROM second_table ORDER BY score DESC;
--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all records with score >= 10 in table second_table
-select score, name from second_table where score >= 10 order by score desc;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;

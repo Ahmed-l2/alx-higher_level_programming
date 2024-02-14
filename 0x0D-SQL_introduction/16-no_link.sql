dc657ddfb292626fc0bc972002feaa80d1538975
Ahmed <ahmed_l2@yahoo.com>
2024-02-14 06:01:18 +0300

    .

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,2 @@
 -- lists all records of the table second_table of the database
-select score, name from second_table where name is not NULL order by score
-desc;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;

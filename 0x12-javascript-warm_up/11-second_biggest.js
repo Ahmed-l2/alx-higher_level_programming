18c48c074187dade3f6404a7cdef0eb8f1cae9e8
Ahmed <ahmed_l2@yahoo.com>
2024-03-12 01:11:00 +0300

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 if (process.argv.length === 2) {
-  console.log('0');
+  console.log(0);
 } else if (process.argv.length === 3) {
-  console.log('0');
+  console.log(0);
 } else {
   let temp = Number(process.argv[2]);
   for (let i = 3; i < process.argv.length; i++) {

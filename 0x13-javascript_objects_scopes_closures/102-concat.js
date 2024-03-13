5012a943a98e44103ccb508a16cd6361c6448b3e
Ahmed <ahmed_l2@yahoo.com>
2024-03-13 03:30:25 +0300

    13. Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,9 +2,11 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function(err, dataA) {
-  fs.readFile(process.argv[3], 'utf8', function(err, dataB) {
-    fs.writeFile(process.argv[4], dataA + dataB, function(err) {
+fs.readFile(process.argv[2], 'utf8', function (err, dataA) {
+  if (err) throw err;
+  fs.readFile(process.argv[3], 'utf8', function (err, dataB) {
+    if (err) throw err;
+    fs.writeFile(process.argv[4], dataA + dataB, function (err) {
       if (err) throw err;
     });
   });

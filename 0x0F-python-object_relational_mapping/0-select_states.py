5895fb2453a99f8e3ad1545c60d73a4c466a5450
Ahmed <ahmed_l2@yahoo.com>
2024-03-17 00:58:31 +0300

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Lists all states from the database `hbtn_0e_0_usa`
 """
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM STATES")
+    cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
 
     for row in rows:

dd20ef3efae12c9cd532d971438d7136c7a875db
Ahmed <ahmed_l2@yahoo.com>
2024-03-17 18:12:45 +0300

    9. Contains

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from sys import argv
+
+
+if __name__ == "__main__":
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine_url = f"mysql+mysqldb://{user}:{password}@localhost:3306/{database}"
+    engine = create_engine(engine_url)
+
+    session = sessionmaker(bind=engine)()
+
+    states = session.query(State).filter(State.name.contains('a'))
+
+    for state in states:
+        print("{state.id}: {state.name}")

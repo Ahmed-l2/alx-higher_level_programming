2e8917e4007256700230442d42cb2b4000f99c79
Ahmed <ahmed_l2@yahoo.com>
2024-03-17 19:57:22 +0300

    13. Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -19,8 +19,6 @@ if __name__ == "__main__":
 
     session = sessionmaker(bind=engine)()
 
-    for state in session.query(State):
-        if 'a' in state.name:
-            session.delete(state)
-
+    session.query(State).filter(State.name.contains('a')).\
+        delete(synchronize_session='fetch')
     session.commit()

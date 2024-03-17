ac62b048cf250143f475e56c9ce8e530a2063cc3
Ahmed <ahmed_l2@yahoo.com>
2024-03-18 02:50:34 +0300

    11. Add a new state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -24,5 +24,5 @@ if __name__ == "__main__":
     new_state = State(name='Louisiana')
     session.add(new_state)
     session.commit()
-    print("{}".format(new_state.id))
+    print('{0}'.format(new_state.id))
     session.close()

7a728328f725d796f8923620590dbc418c7c9930
Ahmed <ahmed_l2@yahoo.com>
2024-03-28 20:16:51 +0300

    1. Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """fetches https://alx-intranet.hbtn.io"""
 import urllib.request
-
+from sys import argv
 
 def fetch():
     """Fetches X-Request-Id variable"""
-    url = "https://alx-intranet.hbtn.io"
+    url = argv[1]
     with urllib.request.urlopen(url) as response:
         content = response.getheader("X-Request-Id")
 

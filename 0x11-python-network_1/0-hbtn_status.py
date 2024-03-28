d20a1fc521c59f3291003b96705717c5f6f9fe49
Ahmed <ahmed_l2@yahoo.com>
2024-03-28 19:59:28 +0300

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,6 +3,7 @@
 import urllib.request
 
 def fetch():
+    """Fetches given url"""
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         html = response.read()

11b9069199d331aaac78a4290218c468f7053f8f
Ahmed <ahmed_l2@yahoo.com>
2024-03-28 19:58:51 +0300

    0. What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+def fetch():
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        d = html.decode('utf-8')
+
+    print("Body response:$")
+    print("    - type: {}$".format(type(html)))
+    print("    - content: {}$".format(html))
+    print("    - utf8 content: {}$".format(d))
+
+if __name__ == "__main__":
+    fetch()

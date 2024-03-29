e3453dbbe553b99bdee344e27d930df70cb44593
Ahmed <ahmed_l2@yahoo.com>
2024-03-29 04:58:58 +0300

    9. My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Module"""
-from sys import requests
+from sys import argv
 import requests
 from requests.auth import HTTPBasicAuth
 
 
 def gitHubCred():
     """GitHub credentials and uses the GitHub API to display your id"""
-    credauth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    credauth = HTTPBasicAuth(argv[1], argv[2])
     response = requests.get("https://api.github.com/user", auth=auth)
     d = response.json()
     print(d.get("id"))

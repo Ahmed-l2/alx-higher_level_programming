ba9aa56958cb820f07701be61db09f41794d1236
Ahmed <ahmed_l2@yahoo.com>
2024-03-29 04:04:43 +0300

    7. Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,13 +7,12 @@ import sys
 def fetch():
     """Fetches URL and display body or status code"""
     url = sys.argv[1]
-
-    response = requests.get(url)
-
-    if response.status_code >= 400:
-        print('Error code:', response.status_code)
-    else:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
         print(response.text)
+    except requests.HTTPError as err:
+        print(f"Error code: {err.response.status_code}")
 
 
 if __name__ == "__main__":
